c3d419485693b3cf4628873e7a4042e696a1c2b5
Naomie <naomie.colombo@gmail.com>
2020-05-20 19:49:06 +0200

    travis file config + setting.py

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dsnlkfsnfienflsnkldflsdj865'
+SECRET_KEY = os.environ.get('S_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
